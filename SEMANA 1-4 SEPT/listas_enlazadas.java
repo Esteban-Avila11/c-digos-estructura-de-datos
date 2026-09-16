d91c0ae03cd3c4ef829f7999dc99da05d9f922e0
Esteban Avila <jaidereavilav@gmail.com>
2026-09-15 19:37:46 -0500

    Update list operations in listas_enlazadas.java
    Fixed method calls for ArrayList operations and corrected method usage.

--- SEMANA 1-4 SEPT/listas_enlazadas.java
+++ SEMANA 1-4 SEPT/listas_enlazadas.java
@@ -1,1 +1,50 @@
+package sesion.interfaz.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class SesionInterfazList {
+
+    public static void imprimirList(List<String> List) {
+                
+        for(int i = 0 ; i < List.size(); i++)
+        {
+            System.out.print(" imprimiendo elemento ");
+            System.out.print(i);
+            System.out.print(" ");
+            System.out.print(List.get(i));
+            System.out.print("\n");
+        }  
+    }
+    
+    public static void main(String[] args) {
+        
+        List<String> List = new ArrayList<>();
+        List.add(" cadena1 ");
+        List.add(" cadena2 ");
+        List.add(" cadena3 ");
+        List.add(" cadena4 ");
+        List.add(" cadena5 ");
+        
+        imprimirList(List);
+        
+        List.addFirst(" string de prueba ");
+        
+        imprimirList(List);
+        
+        List.set(3, " cambiando strings ");
+        
+        imprimirList(List);
+        
+        List.remove(5);
+        
+        imprimirList(List);
+        
+        List.removeFirst();
+        
+        imprimirList(List);
+    }    
+    
+    
+    
+}
