ab55cbf44bab2be2d15d5134566c303e26ab9959
Esteban Avila <jaidereavilav@gmail.com>
2026-09-21 21:31:55 -0500

    Implement LinkedList operations in Actividad_listaEnlazada
    Added elements to the LinkedList and displayed the list.

--- SEMANA 21- 25 SEP/Actividad_listaEnlazada.java
+++ SEMANA 21- 25 SEP/Actividad_listaEnlazada.java
@@ -1,1 +1,26 @@
+package listasenlazadas21sep;
+
+import java.util.LinkedList;
+
+public class ListasEnlazadas21sep {
+
+    public static void main(String[] args) {
+        
+        LinkedList<String> materias = new LinkedList<>();
+        materias.add("matematicas");
+        materias.add("ingles");
+        materias.add("redes");
+        materias.add("bases de datos");
+        materias.add("programacion");
+        
+        materias.forEach(System.out::println);
+        
+        materias.addFirst("algoritmos");
+        materias.addLast("inteligencia artificial");
+        
+        System.out.println("\n Se agrego algoritmos al inicio de la lista e inteligencia artificial al final");
+        
+        materias.forEach(System.out::println);
+    }
+}
 
